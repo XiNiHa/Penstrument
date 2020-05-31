e078c29f4d8a9740055698f5baaec1b064b3f44e
Iha Shin <anesin1109@naver.com>
2020-05-31 23:13:10 +0900

    Initial Commit

---
+++ Penstrument_Win32/Penstrument_Win32/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using TobiasErichsen.teVirtualMIDI;
+
+namespace Penstrument_Win32
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        TeVirtualMIDI midiPort = new TeVirtualMIDI("Penstrument MIDI Port");
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            tabletCanvas.PointerMoved += (sender, e) =>
+            {
+                var currentPoint = e.GetCurrentPoint(tabletCanvas);
+                pointer.SetValue(Canvas.LeftProperty, currentPoint.Position.X - 25);
+                pointer.SetValue(Canvas.TopProperty, currentPoint.Position.Y - 25);
+                outerCircle.Width = 100 * (currentPoint.Properties.Pressure + 1);
+                outerCircle.Height = 100 * (currentPoint.Properties.Pressure + 1);
+                midiPort.sendCommand(new byte[] { 0b11100000, 0, (byte)(currentPoint.Position.X / tabletCanvas.ActualWidth * 127) });
+                midiPort.sendCommand(new byte[] { 0b10110000, 0, (byte)(currentPoint.Position.Y / tabletCanvas.ActualHeight * 127) });
+            };
+        }
+
+        private void PulseCC0(object sender, RoutedEventArgs e)
+        {
+            midiPort.sendCommand(new byte[] { 0b10110000, 0, 127 / 2 });
+        }
+    }
+}
