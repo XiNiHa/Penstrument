6226b72bbbca3c50b4385140d199fb8c7cfd9ed4
Iha Shin <anesin1109@naver.com>
2020-06-07 23:56:38 +0900

    Adjust Pointer Appearancte

--- Penstrument_Win32/Penstrument_Win32/MainWindow.xaml.cs
+++ Penstrument_Win32/Penstrument_Win32/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using System;
 using TobiasErichsen.teVirtualMIDI;
 
 namespace Penstrument_Win32
@@ -19,8 +18,8 @@ namespace Penstrument_Win32
             tabletCanvas.PointerMoved += (sender, e) =>
             {
                 var currentPoint = e.GetCurrentPoint(tabletCanvas);
-                pointer.SetValue(Canvas.LeftProperty, currentPoint.Position.X - 25);
-                pointer.SetValue(Canvas.TopProperty, currentPoint.Position.Y - 25);
+                pointer.SetValue(Canvas.LeftProperty, currentPoint.Position.X - 50);
+                pointer.SetValue(Canvas.TopProperty, currentPoint.Position.Y - 50);
                 outerCircle.Width = 100 * (currentPoint.Properties.Pressure + 1);
                 outerCircle.Height = 100 * (currentPoint.Properties.Pressure + 1);
                 midiPort.sendCommand(new byte[] { 0b11100000, 0, (byte)(currentPoint.Position.X / tabletCanvas.ActualWidth * 127) });
@@ -32,5 +31,6 @@ namespace Penstrument_Win32
         {
             midiPort.sendCommand(new byte[] { 0b10110000, 0, 127 / 2 });
         }
+
     }
 }
