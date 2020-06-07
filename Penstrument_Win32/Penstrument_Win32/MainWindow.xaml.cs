c01471af5b8bfd15dad7e0521b73c6c7dc326ae5
Iha Shin <anesin1109@naver.com>
2020-06-08 03:19:38 +0900

    Relative UI With Some Code Structure Change

--- Penstrument_Win32/Penstrument_Win32/MainWindow.xaml.cs
+++ Penstrument_Win32/Penstrument_Win32/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using TobiasErichsen.teVirtualMIDI;
 
 namespace Penstrument_Win32
@@ -15,21 +16,49 @@ namespace Penstrument_Win32
         {
             this.InitializeComponent();
 
-            tabletCanvas.PointerMoved += (sender, e) =>
-            {
-                var currentPoint = e.GetCurrentPoint(tabletCanvas);
-                pointer.SetValue(Canvas.LeftProperty, currentPoint.Position.X - 50);
-                pointer.SetValue(Canvas.TopProperty, currentPoint.Position.Y - 50);
-                outerCircle.Width = 100 * (currentPoint.Properties.Pressure + 1);
-                outerCircle.Height = 100 * (currentPoint.Properties.Pressure + 1);
-                midiPort.sendCommand(new byte[] { 0b11100000, 0, (byte)(currentPoint.Position.X / tabletCanvas.ActualWidth * 127) });
-                midiPort.sendCommand(new byte[] { 0b10110000, 0, (byte)(currentPoint.Position.Y / tabletCanvas.ActualHeight * 127) });
-            };
+            Activated += OnActivated;
+            innerFrame.PointerMoved += OnPointerMoved;
+        }
+
+        private void OnActivated(object sender, WindowActivatedEventArgs e)
+        {
+            UpdateSizes(Bounds.Height, Bounds.Width);
+
+            SizeChanged += (sender, e) => UpdateSizes(e.Size.Height, e.Size.Width);
+        }
+
+        private void OnPointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            var currentPoint = e.GetCurrentPoint(innerFrame);
+            pointer.SetValue(Canvas.LeftProperty, currentPoint.Position.X - 70);
+            pointer.SetValue(Canvas.TopProperty, currentPoint.Position.Y - 70);
+            outerCircle.Width = 100 * (currentPoint.Properties.Pressure + 1);
+            outerCircle.Height = 100 * (currentPoint.Properties.Pressure + 1);
+            midiPort.sendCommand(new byte[] { 0b11100000, 0, (byte)(currentPoint.Position.X / innerFrame.ActualWidth * 127) });
+            midiPort.sendCommand(new byte[] { 0b10110000, 0, (byte)(currentPoint.Position.Y / innerFrame.ActualHeight * 127) });
+            midiPort.sendCommand(new byte[] { 0b10110000, 1, (byte)(currentPoint.Properties.Pressure * 127) });
+        }
+
+        private void UpdateSizes(double rootWidth, double rootHeight)
+        {
+            theCanvas.Width = rootWidth / 5 * 4;
+            theCanvas.Height = rootHeight;
+
+            outerFrame.Width = theCanvas.Width - 20;
+            outerFrame.Height = theCanvas.Height - 20;
+
+            innerFrame.Width = outerFrame.Width - 40;
+            innerFrame.Height = outerFrame.Height - 40;
         }
 
         private void PulseCC0(object sender, RoutedEventArgs e)
         {
             midiPort.sendCommand(new byte[] { 0b10110000, 0, 127 / 2 });
+        }
+
+        private void PulseCC1(object sender, RoutedEventArgs e)
+        {
+            midiPort.sendCommand(new byte[] { 0b10110000, 1, 127 / 2 });
         }
 
     }
